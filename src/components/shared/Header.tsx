1bc546068927cade1e5db259baf7c8b55bd3e465
miltonzage222 <miltongunza222@gmail.com>
2026-02-22 03:38:22 -0300

    Atualiza layout e adiciona componentes da landing page

---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,25 @@
+import logobarbearia from '../../assets/logo-barbearia.png';
+const Header = () => {
+    return (
+        <header className="flex justify-between items-center px-6 py-4 fixed top-0 left-0 right-0 bg-[#0A0A0A] backdrop-blur-sm border-b border-[rgba(212,175,55.0,0.2)] z-50">
+            <div className="flex items-center gap-2 ">
+                <img src={logobarbearia} alt="Logo da Barbearia Premium" className='w-10 h-10 rounded object-cover' />
+                <span className="text-white">Barbearia Imperador Gunza</span>
+            </div>
+            <nav className='flex items-center'>
+
+                <ul className=' flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8 w-full md:w-auto '>
+                    <li className='text-white hover:text-[#D4Af37] transition-all  duration-300 text-left md:text-center py-2 md:py-0 cursor-pointer'>Inicio</li>
+                    <li className='text-white hover:text-[#D4Af37] transition-all  duration-300 text-left md:text-center py-2 md:py-0 cursor-pointer'>Serviços</li>
+                    <li className='text-white hover:text-[#D4Af37] transition-all  duration-300 text-left md:text-center py-2 md:py-0 cursor-pointer'>Contato</li>
+                </ul>
+
+            </nav>
+
+        </header>
+
+    );
+
+}
+
+export default Header;
